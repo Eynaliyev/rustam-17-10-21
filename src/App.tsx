d7a5975dec4577b017371b49d5766e813b13bc68
Eynaliyev <rustam.eynaliyev@gmail.com>
2021-10-16 19:49:56 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -15,7 +15,7 @@ function App() {
         <span>
           <span>Learn </span>
           <a
-            className="App-link"
+            className="app-link"
             href="https://reactjs.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -24,7 +24,7 @@ function App() {
           </a>
           <span>, </span>
           <a
-            className="App-link"
+            className="app-link"
             href="https://redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function App() {
           </a>
           <span>, </span>
           <a
-            className="App-link"
+            className="app-link"
             href="https://redux-toolkit.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -42,7 +42,7 @@ function App() {
           </a>
           ,<span> and </span>
           <a
-            className="App-link"
+            className="app-link"
             href="https://react-redux.js.org/"
             target="_blank"
             rel="noopener noreferrer"
@@ -51,6 +51,8 @@ function App() {
           </a>
         </span>
       </header>
+      <section></section>
+      <footer></footer>
     </div>
   );
 }
